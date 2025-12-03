748af5a9c0fd966df1bbb58469b75a30d76709dc
Mounika Kurma <mounikakurma25@gmail.com>
2025-12-04 00:22:47 +0530

    Login fragment ui designed

--- app/src/main/java/com/example/mymarketapp/LoginFragment.kt
+++ app/src/main/java/com/example/mymarketapp/LoginFragment.kt
@@ -1,19 +1,28 @@
 package com.example.mymarketapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.mymarketapp.databinding.FragmentLoginBinding
 
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
-class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        _binding = FragmentLoginBinding.bind(view)
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassword.text.toString()
+
+            if (email.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(requireContext(), "Enter credentials", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Login Successful!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
